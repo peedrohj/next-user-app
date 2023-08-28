1df67d1e79996af5f1b8670fdb468018fafb9ab7
Pedro Farias <phjfg@cesar.org.br>
2023-08-28 00:18:18 +0000

    feat: hash password before save user

--- code/lib/actions/createUser.actions.ts
+++ code/lib/actions/createUser.actions.ts
@@ -2,6 +2,7 @@
 
 import User from "../db/models/user.model";
 import { connectToDb } from "../db/mongoose";
+import bcrypt from "bcrypt";
 
 interface createUserParams {
   name: string;
@@ -9,21 +10,16 @@ interface createUserParams {
   password: string;
 }
 
-export async function createUser({
-  name,
-  email,
-  password,
-}: createUserParams) {
+export async function createUser({ name, email, password }: createUserParams) {
   connectToDb();
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    await User.create(
-      {
-        name,
-        email,
-        password,
-      },
-    );
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
   } catch (error: any) {
     throw new Error(`Failed to update user: ${error.message}`);
   }
