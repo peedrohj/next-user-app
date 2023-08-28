ade53e34b1926f32e8565326f3d81a2d5cb4b82e
Pedro Farias <phjfg@cesar.org.br>
2023-08-28 00:58:18 +0000

    feat: create signup page

--- code/app/page.tsx
+++ code/app/page.tsx
@@ -1,11 +1,21 @@
 import CreateAccount from "@/components/forms/CreateAccount";
+import bg from "@/public/assets/bg.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section>
+    <main className="flex min-h-screen md:flex-row min-w-full flex-col-reverse items-center justify-between">
+      <div
+        className="bg-black min-h-screen lg:w-3/5 md:w-2/5 w-full"
+        style={{
+          backgroundImage: `url(${bg.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat"
+        }}
+      ></div>
+      <div className="bg-black flex min-h-screen lg:w-2/5  md:w-3/5 md:px-24 w-full flex-row items-center justify-center ">
         <CreateAccount />
-      </section>
+      </div>
     </main>
   );
 }
